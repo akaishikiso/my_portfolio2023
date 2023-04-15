c50ecd67a13ba81f4b09d5ccb30763c488b153dc
YAMADA Riko <lycopene109@outlook.jp>
2023-04-15 11:11:59 +0900

    WIP

---
+++ portfolio/pages/api/about-me/index.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const AboutMe = () => {
+  return (
+    <>
+      <h1>About me</h1>
+      <Link href={"/"}>Topへ戻る</Link>
+    </>
+  );
+};
+
+export default AboutMe;
